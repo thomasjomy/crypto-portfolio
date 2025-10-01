4d79077abd3204e2a813266b9dc1906790cdc148
Jomy Thomas <jomythomas001@gmail.com>
2025-10-01 14:53:23 +0530

    portfolio service setup

---
+++ portfolio-service/src/test/java/in/jt/crypto/portfolio/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.jt.crypto.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
